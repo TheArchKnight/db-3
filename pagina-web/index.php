c4655ea6591ac23c039599f87cd44e71360aacaa
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2023-11-24 22:42:59 -0500

    test

--- pagina-web/index.php
+++ pagina-web/index.php
@@ -14,7 +14,7 @@
 <body>
 <div class="container mt-5">
 
-    <h1>Trabajo final BD1 - (Título de su trabajo)</h1>
+    <h1>Trabajo final BD1 (hola) - (Título de su trabajo)</h1>
 
     <div class="informacion">
         <ul>
