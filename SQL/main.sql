32e1f175036049d11b16945b76f548de9e5de9ca
Sebastian Soto Arcila <111089192+SebastianSotoAr@users.noreply.github.com>
2023-11-26 17:20:40 -0500

    FINAL main.sql

--- SQL/main.sql
+++ SQL/main.sql
@@ -4,14 +4,14 @@ USE exterminator;
 
 CREATE TABLE cliente(
    correo VARCHAR(25) PRIMARY KEY,
-   telefono NUMERIC(10),
-   direccion VARCHAR(40),
-   nombre VARCHAR(20)
+   telefono NUMERIC(10) NOT NULL,
+   direccion VARCHAR(40) NOT NULL,
+   nombre VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE departamento(
    codigo INT PRIMARY KEY,
-   numero_empleados INT
+   numero_empleados INT NOT NULL
 );
 
 CREATE TABLE grupo_fumigacion(
@@ -24,14 +24,14 @@ CREATE TABLE oficina(
 
 CREATE TABLE empleado(
    cedula INT PRIMARY KEY,
-   nombre_completo VARCHAR(50),
-   salario NUMERIC(15),
-   telefono NUMERIC(10)
+   nombre_completo VARCHAR(50) NOT NULL,
+   salario NUMERIC(15) NOT NULL,
+   telefono NUMERIC(10) NOT NULL
 );
 
 CREATE TABLE oficinista(
    cedula_empleado INT PRIMARY KEY REFERENCES empleado,
-   oficina INT REFERENCES oficina
+   oficina INT REFERENCES oficina NOT NULL
 );
 
 CREATE table empleado_de_grupo(
@@ -40,32 +40,32 @@ CREATE table empleado_de_grupo(
 
 CREATE TABLE cotizacion(
    codigo INT PRIMARY KEY,
-   monto VARCHAR(15),
-   detalle VARCHAR(255)
+   monto VARCHAR(15) NOT NULL,
+   detalle VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE visita(
    codigo INT PRIMARY KEY,
-   fecha DATE,
-   hora VARCHAR(5), -- es una clave primaria candidata
-   costo VARCHAR(50),
-   tipo VARCHAR(20),
-   reporte VARCHAR(250),
-   observaciones VARCHAR(50),
-   codigo_departamento INT REFERENCES grupo_fumigacion,
+   fecha DATE NOT NULL,
+   hora VARCHAR(5) NOT NULL, -- es una clave primaria candidata
+   costo VARCHAR(50) NOT NULL,
+   tipo VARCHAR(20) NOT NULL,
+   reporte VARCHAR(250) NOT NULL,
+   observaciones VARCHAR(50) NOT NULL,
+   codigo_departamento INT REFERENCES grupo_fumigacion NOT NULL,
    codigo_cotizacion INT REFERENCES cotizacion,
 );
 
-ALTER TABLE cotizacion ADD codigo_proxima_visita INT REFERENCES cotizacion;
+ALTER TABLE cotizacion ADD codigo_proxima_visita INT UNIQUE REFERENCES cotizacion;
 
-CREATE TABLE llamada(
+CREATE TABLE llamada (
    codigo INT PRIMARY KEY,
-   fecha DATE,
-   hora VARCHAR(5),
-   correo_cliente VARCHAR(25) REFERENCES cliente,
-   cedula_empleado INT REFERENCES oficinista,
-   codigo_cotizacion INT REFERENCES cotizacion,
-   codigo_visita INT REFERENCES visita,
+   fecha DATE NOT NULL,
+   hora VARCHAR(5) NOT NULL,
+   correo_cliente VARCHAR(25) REFERENCES cliente NOT NULL,
+   cedula_empleado INT REFERENCES oficinista NOT NULL,
+   codigo_cotizacion INT UNIQUE REFERENCES cotizacion,
+   codigo_visita INT UNIQUE REFERENCES visita,
    CHECK (
       (codigo_cotizacion IS NULL AND codigo_visita IS NOT NULL)
       OR
@@ -75,26 +75,26 @@ CREATE TABLE llamada(
 
 CREATE TABLE permiso(
    clave_acceso INT PRIMARY KEY,
-   nivel INT,
-   descripcion VARCHAR(225)
+   nivel INT NOT NULL,
+   descripcion VARCHAR(225) NOT NULL
 );
 
 CREATE TABLE tarea (
    clave INT PRIMARY KEY,
-   descripcion VARCHAR(220),
-   codigo_departamento INT REFERENCES grupo_fumigacion,
+   descripcion VARCHAR(220) NOT NULL,
+   codigo_departamento INT REFERENCES grupo_fumigacion NOT NULL,
    codigo_tarea INT UNIQUE REFERENCES tarea,
-   clave_permiso INT UNIQUE REFERENCES permiso,
+   clave_permiso INT UNIQUE REFERENCES permiso NOT NULL,
 );
 
 CREATE TABLE producto(
    codigo INT PRIMARY KEY,
-   tipo VARCHAR(25),
-   nombre VARCHAR(25),
-   descripcion VARCHAR(225),
-   especificaciones_uso VARCHAR(250),
-   fabricante VARCHAR(25),
-   nivel_acceso INT,
+   tipo VARCHAR(25) NOT NULL,
+   nombre VARCHAR(25) NOT NULL,
+   descripcion VARCHAR(225) NOT NULL,
+   especificaciones_uso VARCHAR(250) NOT NULL,
+   fabricante VARCHAR(25) NOT NULL,
+   nivel_acceso INT NOT NULL,
    clave_permiso INT REFERENCES permiso,
    codigo_visita INT REFERENCES visita,
 );
