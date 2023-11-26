10fca9df7576b990a9ed3d886495243e6696d336
Sebastian Soto Arcila <111089192+SebastianSotoAr@users.noreply.github.com>
2023-11-26 17:29:24 -0500

    final final main.sql

--- SQL/main.sql
+++ SQL/main.sql
@@ -54,6 +54,7 @@ CREATE TABLE visita(
    observaciones VARCHAR(50) NOT NULL,
    codigo_departamento INT REFERENCES grupo_fumigacion NOT NULL,
    codigo_cotizacion INT REFERENCES cotizacion,
+   CHECK(tipo = 'realizada' OR tipo = 'sin realizar')
 );
 
 ALTER TABLE cotizacion ADD codigo_proxima_visita INT UNIQUE REFERENCES cotizacion;
@@ -83,8 +84,9 @@ CREATE TABLE tarea (
    clave INT PRIMARY KEY,
    descripcion VARCHAR(220) NOT NULL,
    codigo_departamento INT REFERENCES grupo_fumigacion NOT NULL,
-   codigo_tarea INT UNIQUE REFERENCES tarea,
+   clave_tarea INT UNIQUE REFERENCES tarea,
    clave_permiso INT UNIQUE REFERENCES permiso NOT NULL,
+   CHECK(clave != clave_tarea)
 );
 
 CREATE TABLE producto(
@@ -97,6 +99,7 @@ CREATE TABLE producto(
    nivel_acceso INT NOT NULL,
    clave_permiso INT REFERENCES permiso,
    codigo_visita INT REFERENCES visita,
+   CHECK (tipo = 'herramienta' OR tipo = 'insumo')
 );
 
 CREATE TABLE empleado_en_grupo(
